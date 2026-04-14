7ae2f3104afa3bd947a450f96d8975d64cbc82a8
shizq <yezi_1490@163.com>
2026-04-14 13:18:44 +0800

    add analysis page

--- angular-app/chunk-XEH2RCKH.js
+++
@@ -1,2 +0,0 @@
-import{Ib as o,Jb as s,ec as i,ib as e}from"./chunk-TF762ZRF.js";var m=class n{constructor(){}ngOnInit(){}static \u0275fac=function(t){return new(t||n)};static \u0275cmp=e({type:n,selectors:[["app-settings"]],decls:2,vars:0,template:function(t,a){t&1&&(o(0,"p"),i(1,` settings works!
-`),s())},encapsulation:2})};export{m as SettingsComponent};

4329e612f5488c48c0688946b9e467ded461da73
Kelly Shi <86490151@qq.com>
2026-07-26 15:22:00 +0800

    feat: add role page content

--- react-admin-system/assets/DocumentsPage-koR-hYBH.js
+++
@@ -1,1 +0,0 @@
-import{R as e,t}from"./Card-BfR1i05S.js";var n=e(),r=()=>(0,n.jsx)(t,{title:`文档管理`,bordered:!1,children:(0,n.jsx)(`div`,{children:`文档管理页面 - 动态路由控制`})});export{r as DocumentsPage,r as default};
